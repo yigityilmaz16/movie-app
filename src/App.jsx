06feee4993c6b74b4b620891444e9b062ddb8a55
yigityilmaz16 <yigityilmaz196345@gmail.com>
2026-07-13 12:47:32 +0300

    feat: add loading and error states

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,43 @@ function App(){
   const [movies, setMovies] = useState([]);
   const [searchTerm,setSearchTerm] = useState("");
   const [loading,setLoading] = useState(false);
+  const [error,setError] = useState("");
 
-  function searchFilm(searchTerm){
-    setLoading(true);
-    fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(searchTerm)}&language=tr-TR&page=1`, {
-    headers: {
-      Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
-      accept: "application/json",
-    },
-  })
+ function searchFilm(searchTerm) {
+  setError("");
+  setLoading(true);
+
+  fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(searchTerm)}&language=tr-TR&page=1`,
+    {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+        accept: "application/json",
+      },
+    }
+  )
     .then((response) => {
+      if (!response.ok) {
+        throw new Error("API isteği başarısız oldu");
+      }
+
       return response.json();
     })
     .then((data) => {
       setMovies(data.results);
+    })
+    .catch(() => {
+      setError("Filmler yüklenirken bir hata oluştu.");
+    })
+    .finally(() => {
+      setLoading(false);
     });
-     setLoading(false);
-  }
+}
 
 
  useEffect(() => {
   setLoading(true);
+  setError("");
   fetch("https://api.themoviedb.org/3/movie/popular?language=tr-TR&page=1", {
     headers: {
       Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
@@ -36,19 +52,28 @@ function App(){
     },
   })
     .then((response) => {
+      if(!response.ok){
+        throw new Error("API isteği başarısız oldu");
+      }
       return response.json();
     })
     .then((data) => {
       setMovies(data.results);
+    })
+    .catch(()=>{
+      setError("Filmler Yüklenirken Bir Hata Oluştu");
+    })
+    .finally(()=>{
+      setLoading(false);
     });
-     setLoading(false);
+     
 }, []);
 
   return(
     <>
     <Header />
     <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} searchFilm={searchFilm} />
-    <MovieList movies={movies} loading={loading}  />
+    <MovieList movies={movies} loading={loading} error={error}  />
     </>
   );
 }
